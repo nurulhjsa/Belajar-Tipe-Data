48d43a13a506a5852a028eb0c87040b6720e115d
nurulhjsa <nurulhasanahbule.nh@gmail.com>
2018-01-03 11:22:46 +0700

    Belajar Beberapa Tipe Data

---
+++ Index.java
@@ -0,0 +1,10 @@
+public class Index{
+    public static void main(String[] args){
+        System.out.println("Nama Saya Adalah : " + TipeData.nama);
+        System.out.println("Umur : " + TipeData.umur);
+        System.out.println("Alamat Tempat Tinggal Adalah : " + TipeData.alamat);
+        System.out.println("Jenis Kelamin Wanita : " + TipeData.wanita);
+        System.out.println("Golongan Darah Adalah : " + TipeData.goldar);
+        System.out.println("Tinggi Badan Adalah : " + TipeData.tinggi);
+    }
+}
---
+++ TipeData.java
@@ -0,0 +1,8 @@
+public class TipeData{
+    public static final String nama = "Nurul Hasanah";
+    public static final int umur = 22;
+    public static final String alamat = "Jl. Bidar Kampus Blok A No.4 Palembang";
+    public static final Boolean wanita = true;
+    public static final char goldar = '0';
+    public static final Double tinggi = 160.5;
+    }
